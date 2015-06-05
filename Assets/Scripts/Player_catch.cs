ed0d636dd7d9df9673b214b9aab305546dcc8aad
Denis Kuznetsov <kuznetsov.den.p@gmail.com>
2015-06-05 19:20:29 +0300

    Add player status

---
+++ Assets/Scripts/Player_catch.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_catch : MonoBehaviour {
+	public bool debag=true;
+	private Vector2 Vect;
+	private RaycastHit2D hit;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch ((int)Input.GetAxis ("Vertical")) {
+		case -1:
+			 Vect = Vector2.up;
+			break;
+		case 1:
+			 Vect = -Vector2.up;
+			break;
+		default:
+			switch ((int)Input.GetAxis ("Horizontal")) {
+			case -1:
+				 Vect = -Vector2.right;
+				break;
+			case 1:
+				Vect = Vector2.right;
+				break;
+			default:
+				return;
+			}
+			break;
+		}
+		if (0f != Input.GetAxis ("Catch")) 
+		if (hit.collider == null) {
+			hit = Physics2D.Raycast (transform.position, Vect,1,2);
+			if(debag) Debug.Log ("catch"+hit.transform.name);
+						if (hit.transform.name == "Load") {
+								hit.transform.rigidbody2D.isKinematic = true;	
+								hit.transform.parent = transform;
+						}
+		} else 
+			if (hit.transform.name == "Load") {
+				hit.transform.rigidbody2D.isKinematic = false;	
+				hit.transform.parent = null;
+			}
+			
+		
+		}
+		
+	
+
+}
---
+++ Assets/Scripts/Player_status.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_status : MonoBehaviour {
+	public GUISkin Skin;
+	public bool enable=true;
+	public float health=100;
+	public float fury=100;
+	public float health_max=100;
+	public float fury_max=100;
+	
+	public float coord_x=10;
+	public float coord_y=10;
+	public float length=254;
+	public float width=64;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnGUI ()
+	{
+		if (enable) {
+			float Health_rel_length=health/health_max;
+			float Fury_rel_length=fury/fury_max;
+			GUI.skin=Skin;
+			GUI.Box(new Rect(coord_x,coord_y,length,width),"",GUI.skin.GetStyle("background"));
+			GUI.Box(new Rect(coord_x,coord_y,length*Health_rel_length,width),"",GUI.skin.GetStyle("health"));
+			GUI.Box(new Rect(coord_x,coord_y,length*Fury_rel_length,width),"",GUI.skin.GetStyle("fury"));
+			GUI.Box(new Rect(coord_x,coord_y,length,width),"",GUI.skin.GetStyle("bar"));
+		}
+		}
+}
