b3a55d6d868c5e0e79504a4ea087edb274a7d85b
IvanKa4kifilm <Ivanka4kifilm@icloud.com>
2015-06-25 22:50:02 +0700

    допилил таймер

--- Assets/Scripts/Animation_player.cs
+++ Assets/Scripts/Animation_player.cs
@@ -14,60 +14,64 @@ public class Animation_player : MonoBehaviour {
 	private Animator anim; 
 	private bool left_flag=false;
 	private bool right_flag=false;
-
+	
 	void Start(){
 		anim = this.GetComponent<Animator> ();
-
+		
 	}
 	// Update is called once per frame
 	void Update () {
 		if(!(Input.GetKey (KeyCode.D)&&Input.GetKey (KeyCode.A))){
-
-																 
-		if (Input.GetKey (KeyCode.D)) {
-						anim.SetBool ("right", true);
-						anim.SetBool ("right_stop", false);
-						anim.SetBool ("left", false);
-						anim.SetBool ("left_stop",false);
-			            right_flag=true;
-						left_flag=false;
-				}
-		else{
-			if(right_flag==true){
-									right_flag=false;
-									anim.SetBool ("right_stop", true);
-									anim.SetBool ("right", false);
-									anim.SetBool ("left", false);
-									anim.SetBool ("left_stop",false);
-								}
-
-			}
-		if (Input.GetKey (KeyCode.A)) {
-			 			anim.SetBool("right",false);
-						anim.SetBool ("left", true);
-						anim.SetBool ("right_stop", false);
-						anim.SetBool ("left_stop",false);
-						right_flag=false;
-						left_flag=true;
-		}
 			
-		else{
-			if(left_flag==true){
-									left_flag=false;
-									anim.SetBool ("left_stop", true);
-									anim.SetBool ("right", false);
-									anim.SetBool ("left", false);
-									anim.SetBool ("right_stop",false);
+			
+			if (Input.GetKey (KeyCode.D)) {
+				anim.SetBool ("right", true);
+				anim.SetBool ("right_stop", false);
+				anim.SetBool ("left", false);
+				anim.SetBool ("left_stop",false);
+				right_flag=true;
+				left_flag=false;
 			}
-
-	}
+			else{
+				if(right_flag==true){
+					right_flag=false;
+					anim.SetBool ("right_stop", true);
+					anim.SetBool ("right", false);
+					anim.SetBool ("left", false);
+					anim.SetBool ("left_stop",false);
+				}
+				
+			}
+			if (Input.GetKey (KeyCode.A)) {
+				anim.SetBool("right",false);
+				anim.SetBool ("left", true);
+				anim.SetBool ("right_stop", false);
+				anim.SetBool ("left_stop",false);
+				right_flag=false;
+				left_flag=true;
+			}
+			
+			else{
+				if(left_flag==true){
+					left_flag=false;
+					anim.SetBool ("left_stop", true);
+					anim.SetBool ("right", false);
+					anim.SetBool ("left", false);
+					anim.SetBool ("right_stop",false);
+				}
+				
+			}
 		}
 		//делаю таймер.
-		if (!(Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.A)|| Input.GetKey (KeyCode.Space))) {
-													mytimer+=Time.deltaTime;
-													Debug.Log("время:"+mytimer);
-				}
-
-}
-			
+		if ((Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.Space))) 
+		{
+			mytimer = 0;
+			Debug.Log ("время:" + mytimer);
+		} 
+		else {
+			mytimer+=Time.deltaTime;
+			Debug.Log ("время:" + mytimer);
+		}
+	}
+	
 }
